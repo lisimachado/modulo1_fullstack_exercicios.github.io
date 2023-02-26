224459ce8b3855221a5165577ef1247158be5b36
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-02-26 11:06:03 -0300

    Preparando espaço para novo desafio

--- ex05_confirm/js/script.js
+++
@@ -1,9 +0,0 @@
-/*button onclick = confirm_envio(){
-	alert("Enviado! Confira no seu e-mail o link de acesso às aulas gratuitas.")
-}
-*/
-
-function confirm(){
-	alert("Enviado! Confira no seu e-mail o link de acesso às aulas gratuitas.")
-}
-button.addEventListener('onclick', confirm);
