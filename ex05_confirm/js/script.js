c1e8844dff66501e1868fbe647ed51f0f195d58c
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-02-26 10:38:23 -0300

    criando branch semana 3.

---
+++ ex05_confirm/js/script.js
@@ -0,0 +1,9 @@
+/*button onclick = confirm_envio(){
+	alert("Enviado! Confira no seu e-mail o link de acesso às aulas gratuitas.")
+}
+*/
+
+function confirm(){
+	alert("Enviado! Confira no seu e-mail o link de acesso às aulas gratuitas.")
+}
+button.addEventListener('onclick', confirm);
