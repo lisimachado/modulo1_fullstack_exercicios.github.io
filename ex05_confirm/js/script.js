28c0c30a1b131990aa358cf423b10886d86e56f9
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-03-11 11:05:03 -0300

    Exercícios 5 e 6 completos

--- ex05_confirm/js/script.js
+++ ex05_confirm/js/script.js
@@ -1,9 +1,20 @@
-/*button onclick = confirm_envio(){
-	alert("Enviado! Confira no seu e-mail o link de acesso às aulas gratuitas.")
-}
-*/
+const btn = document.getElementById("botao");
 
-function confirm(){
-	alert("Enviado! Confira no seu e-mail o link de acesso às aulas gratuitas.")
-}
-button.addEventListener('onclick', confirm);
+btn.addEventListener("click", function(e){
+
+	e.preventDefault(); //se não colocar essa etapa, não salva os dados no console
+
+	const name = document.getElementById("nome");
+	const email = document.getElementById("email");
+
+	const campo1 = name.value;
+	const campo2 = email.value;
+	
+	console.log(campo1, campo2);
+
+	name.value = "";
+	email.value = "";
+
+	alert("Seus dados foram enviados!");
+
+})
