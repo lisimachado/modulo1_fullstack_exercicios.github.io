eceb4ee0b3a7227979463fe9e56aeefd57bf2de6
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-03-11 17:55:04 -0300

    Exercício 9 concluído

---
+++ ex09_console/script.js
@@ -0,0 +1,6 @@
+var nome = String(prompt("Qual seu nome?"));
+var idade = Number(prompt("Qual sua idade?"));
+var cidade = String(prompt("Qual a cidade que você mora?"));
+
+console.log(nome, idade, cidade);
+
