09d075923760b0a8e677b329eed181dc99513809
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-03-19 17:39:35 -0300

    Exercício 3 concluído

---
+++ ex03_array_Arrow/script.js
@@ -0,0 +1,11 @@
+
+var arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var arrayInvertido = [];
+
+var reverseArray = array => {
+	return array.reverse();
+}
+
+console.log(arrayNumeros);
+console.log(reverseArray(arrayNumeros));
