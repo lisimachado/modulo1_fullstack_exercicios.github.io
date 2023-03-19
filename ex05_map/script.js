076603833ec76a7e56c6707398a85868f7345509
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-03-19 17:57:48 -0300

    Exercício 5 concluído

---
+++ ex05_map/script.js
@@ -0,0 +1,9 @@
+
+var arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var arrayQuadrados = [];
+
+const quadrados = numeros => numeros.map(x => x ** 2);
+			
+console.log(arrayNumeros);
+console.log(quadrados(arrayNumeros));
